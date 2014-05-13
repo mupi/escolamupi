d4e99d06b9f6633226c33102f4ebc7340d639b5e
Virgilio N Santos <virgilio.santos@gmail.com>
2014-05-13 16:54:59 -0300

    Payment: build payment complete flow (step by step approach)

--- payments/views.py
+++ payments/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
 from django.core.urlresolvers import reverse
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from django.conf import settings
 
 from paypal.standard.forms import PayPalPaymentsForm
 from braces.views import LoginRequiredMixin
 
-from django.core.urlresolvers import reverse
+import paypalrestsdk
+from .models import (Plans, UserPlanData)
+
+from django.forms import ModelForm
+
 
 class AccountPaymentView(LoginRequiredMixin, TemplateView):
 	template_name = "payment.html"
@@ -41,14 +46,60 @@ class AccountPaymentView(LoginRequiredMixin, TemplateView):
 
 		return self.render_to_response(context)
 
-class PlansView(LoginRequiredMixin, TemplateView):
+	def dispatch(self, *args, **kwargs):
+		from django.shortcuts import redirect
+		try:
+			upd = UserPlanData.objects.get(user_id=self.request.user.id)
+		except:
+			return redirect('/accounts/plans', *args, **kwargs)
+		else:
+			if upd.user_status:
+				return redirect('/my-courses', *args, **kwargs)
+			else:
+				return super(AccountPaymentView,
+						self).dispatch(*args, **kwargs)
+
+
+class PlansView(LoginRequiredMixin, CreateView):
 	template_name = "plans.html"
+	model = UserPlanData
+	fields = ['plan']
+	success_url = '/accounts/payment'
 
 	def get_context_data(self, **kwargs):
-		return super(PlansView, self).get_context_data(**kwargs)
+		context = super(PlansView, self).get_context_data(**kwargs)
+		plans = Plans.objects.all()
+		context['plans'] = plans
+		return context
 
-	def get(self, request, **kwargs):
-		return super(PlansView, self).get(request, **kwargs)
+	def form_valid(self, form):
+		from datetime import datetime
+		import time
+
+		dt = datetime.now()
+		p = Plans.objects.get(id=form.data["plan"])
+		dt = datetime.fromtimestamp(int(time.mktime(dt.timetuple()))
+						+ p.period)
+		form.instance.user_id = self.request.user.id
+		form.instance.expiration_date = dt
+		form.instance.user_status = False
+
+		return super(PlansView, self).form_valid(form)
+
+	def dispatch(self, *args, **kwargs):
+		try:
+			upd = UserPlanData.objects.get(user_id=self.request.user.id)
+		except:
+			pass
+		else:
+			from django.shortcuts import redirect
+			if upd.user_status:
+				return redirect('/my-courses', *args, **kwargs)
+			else:
+				return redirect('/accounts/payment', *args, **kwargs)
+
+		return super(PlansView, self).dispatch(*args, **kwargs)
+
 
 
 
