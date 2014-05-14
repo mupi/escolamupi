a14e5cb5710b8d02a7b1b3615b134600b39e0882
Virgilio N Santos <virgilio.santos@gmail.com>
2014-05-14 11:18:15 -0300

    Payment: Change location of paypal signal

--- payments/models.py
+++ payments/models.py
@@ -3,10 +3,6 @@ from accounts.models import TimtecUser
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
-from paypal.standard.ipn.signals import (payment_was_successful, subscription_signup,
-						payment_was_flagged, payment_was_refunded,
-						payment_was_reversed)
-
 
 class UserPayments (models.Model):
 	payment_id = models.CharField(_('Payment ID'), max_length=30, blank=False, unique=True)
@@ -48,34 +44,5 @@ class UserPlanData (models.Model):
 
 	def __unicode__(self):
 		return self.plan.name
-
-def paypal_signal_was_successful(sender, **kwargs):
-	# Verify if the message is from paypal
-	# https://www.paypal.com/cgi-bin/webscr?cmd=_notify-validate&mc_gross=19.95&protection_eligibility=Eligible
-	# if yes, it will return VERIFIED
-
-	# Verify if the message does not exist already txn_id = 61E67681CH3238416
-
-	send_mail("Uepa", "=)", "contato@mupi.me", ["virgilio.santos@gmail.com"])
-	from urlparse import parse_qs
-	custom = parse_qs(sender.custom)
-	print custom
- 	print sender.txn_type + " -- " + custom['user_id'][0] + " -- " + custom['user_mail'][0]
-	user = TimtecUser.objects.get(id=custom['user_id'][0])
-	print user.email + "--" + custom['user_mail'][0]
-	if sender.payment_status == "Completed":
-		user.is_staff = True
-		user.save()
-
-
-def paypal_signal_subscription_signup(sender, **kwargs):
-	send_mail("Uepa subscription!", "=)", "contato@mupi.me", ["virgilio.santos@gmail.com"])
-
-payment_was_successful.connect(paypal_signal_was_successful)
-subscription_signup.connect(paypal_signal_subscription_signup)
-#payment_was_flagged.connect(paypal_signal_was_successful)
-#payment_was_refunded.connect(paypal_signal_was_successful)
-#payment_was_reversed.connect(paypal_signal_was_successful)
-
 
 
--- payments/views.py
+++ payments/views.py
@@ -8,9 +8,16 @@ from paypal.standard.forms import PayPalPaymentsForm
 from braces.views import LoginRequiredMixin
 
 import paypalrestsdk
+
 from .models import (Plans, UserPlanData)
+from accounts.models import TimtecUser
 
 from django.forms import ModelForm
+
+from paypal.standard.ipn.signals import (payment_was_successful, subscription_signup,
+						payment_was_flagged, payment_was_refunded,
+						payment_was_reversed)
+from django.core.mail import send_mail
 
 
 class AccountPaymentView(LoginRequiredMixin, TemplateView):
@@ -22,29 +29,13 @@ class AccountPaymentView(LoginRequiredMixin, TemplateView):
 		base_url = "http://localhost:8000"
 
 	def get(self, request, **kwargs):
-		u = self.request.user
-
-		settings.PAYPAL_DICT_MONTHLY['custom'] = "user_mail=" + u.email + "&user_id=" + str(u.id)
-		settings.PAYPAL_DICT_YEARLY['custom'] = "user_mail=" + u.email + "&user_id=" + str(u.id)
-
-		settings.PAYPAL_DICT_MONTHLY['notify_url'] = settings.SITE_URL + reverse('paypal-ipn')
-		form_monthly = PayPalPaymentsForm(initial=settings.PAYPAL_DICT_MONTHLY)
-
-		settings.PAYPAL_DICT_YEARLY['notify_url'] = settings.SITE_URL + reverse('paypal-ipn')
-		form_yearly = PayPalPaymentsForm(initial=settings.PAYPAL_DICT_YEARLY)
-
-		if settings.DEBUG:
-			context = {
-				"form_monthly": form_monthly.sandbox(),
-				"form_yearly": form_yearly.sandbox()
-			}
-		else:
-			context = {
-				"form_monthly": form_monthly.sandbox(),
-				"form_yearly": form_yearly.sandbox()
-			}
-
-		return self.render_to_response(context)
+		# Se chegou aqui, o cara tem plano, entao, vamo renderizar o que ele
+		# pode usar pra pagar:
+		# Get all possible providers
+		# Create forms for each provider
+		# Render to response
+		return super(AccountPaymentView,
+				self).get(request, **kwargs)
 
 	def dispatch(self, *args, **kwargs):
 		from django.shortcuts import redirect
@@ -100,6 +91,58 @@ class PlansView(LoginRequiredMixin, CreateView):
 
 		return super(PlansView, self).dispatch(*args, **kwargs)
 
+def paypal_signal_was_successful(sender, **kwargs):
+	# Verify if the message is from paypal
+	# https://www.paypal.com/cgi-bin/webscr?cmd=_notify-validate&mc_gross=19.95&protection_eligibility=Eligible
+	# if yes, it will return VERIFIED
+
+	# Verify if the message does not exist already txn_id = 61E67681CH3238416
+
+	send_mail("Uepa", "=)", "contato@mupi.me", ["virgilio.santos@gmail.com"])
+	from urlparse import parse_qs
+	custom = parse_qs(sender.custom)
+	print custom
+ 	print sender.txn_type + " -- " + custom['user_id'][0] + " -- " + custom['user_mail'][0]
+	user = TimtecUser.objects.get(id=custom['user_id'][0])
+	print user.email + "--" + custom['user_mail'][0]
+	if sender.payment_status == "Completed":
+		user.is_staff = True
+		user.save()
 
 
+def paypal_signal_subscription_signup(sender, **kwargs):
+	print 'oe'
+	send_mail("Uepa subscription!", "=)", "contato@mupi.me", ["virgilio.santos@gmail.com"])
+
+payment_was_successful.connect(paypal_signal_was_successful)
+subscription_signup.connect(paypal_signal_subscription_signup)
+payment_was_flagged.connect(paypal_signal_was_successful)
+#payment_was_refunded.connect(paypal_signal_was_successful)
+#payment_was_reversed.connect(paypal_signal_was_successful)
+
+
+def get_old(self, request, **kwargs):
+	u = self.request.user
+
+	settings.PAYPAL_DICT_MONTHLY['custom'] = "user_mail=" + u.email + "&user_id=" + str(u.id)
+	settings.PAYPAL_DICT_YEARLY['custom'] = "user_mail=" + u.email + "&user_id=" + str(u.id)
+
+	settings.PAYPAL_DICT_MONTHLY['notify_url'] = settings.SITE_URL + reverse('paypal-ipn')
+	form_monthly = PayPalPaymentsForm(initial=settings.PAYPAL_DICT_MONTHLY)
+
+	settings.PAYPAL_DICT_YEARLY['notify_url'] = settings.SITE_URL + reverse('paypal-ipn')
+	form_yearly = PayPalPaymentsForm(initial=settings.PAYPAL_DICT_YEARLY)
+
+	if settings.DEBUG:
+		context = {
+			"form_monthly": form_monthly.sandbox(),
+			"form_yearly": form_yearly.sandbox()
+		}
+	else:
+		context = {
+			"form_monthly": form_monthly.sandbox(),
+			"form_yearly": form_yearly.sandbox()
+		}
+
+	return self.render_to_response(context)
 
