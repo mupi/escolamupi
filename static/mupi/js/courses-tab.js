caa413da17f2acba1a2ce71ca50a07a91efbd6e5
Virgilio N Santos <virgilio.santos@gmail.com>
2014-07-04 11:52:37 -0300

    Courses: add permalink feature to tabs (scrolling top)

--- static/mupi/js/courses-tab.js
+++ static/mupi/js/courses-tab.js
@@ -3,5 +3,15 @@ $(document).ready(function (){
 	  e.preventDefault();
 	  /*$(this).tab('show');*/
 	  $("#web-design-link").trigger("click");
-	})
-})
+	});
+
+    var url = document.location.toString();
+    if (url.match('#')) {
+        jQuery('.tablist li a[href=#'+url.split('#')[1]+']').tab('show') ;
+    } 
+
+    jQuery('.tablist li a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+        window.scrollTo(0, 0);
+    });
+});
